dbec893866f5cbb1aff0c766dcbe7adb00731b96
Mohammed Moosa Sharieff <moosasharieff@gmail.com>
2023-11-01 16:28:37 +0000

    EYES-3 testing 4

--- scrapper/__init__.py
+++ scrapper/__init__.py
@@ -5,3 +5,5 @@
 print('Alhamdulillah!')
 print("Alhamdulillah! Check if the built initiates after pushing the commit.")
 print("Made changes in the GitHub Webhook Configuration, Insha Allah, it should work now.")
+print('Alhamdulillah, identified that GitHub WebHook will not work on local machine. So Masha Allah, created an ec2 '
+      'instance for testing GitHub Webhook Configuration; Insha Allah, it should work now.')
