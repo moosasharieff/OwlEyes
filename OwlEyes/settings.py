68521fdfa9ce921943f758e5f2cd95dae12efa07
Mohammed Moosa Sharieff <moosasharieff@gmail.com>
2023-11-21 14:00:29 +0000

    EYES-21 Created base.html and homepage for the application

--- OwlEyes/settings.py
+++ OwlEyes/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+SCRAPPER_DIR = Path.joinpath(BASE_DIR, 'scrapper/templates/')
 
 
 # Quick-start development settings - unsuitable for production
@@ -55,7 +56,9 @@ ROOT_URLCONF = 'OwlEyes.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [BASE_DIR / 'templates',
+                 SCRAPPER_DIR,
+                 ]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
